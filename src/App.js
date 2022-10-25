fe326318432de32cb72a784b407194f61af61317
DESKTOP-N3U3U1N\hli2 <hli2@tcs.woolworths.com.au>
2022-10-25 21:17:35 +1100

    completed get and delete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React,  { useEffect, useState } from 'react';
+
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+import axios from 'axios';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
 
 function App() {
+  const url = 'http://universities.hipolabs.com/search?country=Australia'
+  const [loaded,setLoaded] = useState(false)
+  const [post, setPost] = useState(null);
+
+  useEffect(()=>{
+    axios.get(`${url}`).then((response)=>{
+      setPost(response.data)
+    })
+  },[])
+  if (!post) return null;
+
+  
+  
+  function createData(domains, country, name, stateProvince, web_pages,alpha_two_code) {
+    return { domains, country, name, stateProvince, web_pages,alpha_two_code };
+  }
+
+
+  const loadData = ()=>{
+    console.log('post', post)
+
+    setLoaded(true)
+  }
+
+  const delData = ()=>{
+    console.log('del data')
+    setPost(post.slice(0,-1))
+    
+  }
+  
+  const addData = ()=>{
+    console.log('add data')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  style={{ height: 400, width: '100%' }}>
+
+<TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell align="left">domains</StyledTableCell>
+            <StyledTableCell align="left">country</StyledTableCell>
+            <StyledTableCell align="left">name</StyledTableCell>
+            <StyledTableCell align="right">stateProvince</StyledTableCell>
+            <StyledTableCell align="left">web_pages</StyledTableCell>
+            <StyledTableCell align="right">alpha_two_code</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {loaded===true && post.map((row) => (
+            <StyledTableRow key={parseInt(Date.now() * Math.random())}>
+              
+              <StyledTableCell align="left">{row.domains.join(`\n`) }</StyledTableCell>
+              <StyledTableCell align="left">{row.country}</StyledTableCell>
+              <StyledTableCell align="left">{row.name}</StyledTableCell>
+              <StyledTableCell align="right">{row.stateProvince}</StyledTableCell>
+              <StyledTableCell align="left">{row.web_pages.join(`\n`)}</StyledTableCell>
+              <StyledTableCell align="right">{row.alpha_two_code}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+
+
+      <button onClick={loadData} >Load </button>
+      <button onClick={delData} >DELETE</button>
+      <button onClick={addData} >ADD</button>
+      
     </div>
   );
 }
